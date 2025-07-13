704b032a65ac9f1fb94621fb5948bec4e36920db
lyy <1411636691@qq.com>
2025-07-13 11:32:24 +0800

    添加项目文件。

---
+++ DataNodeCenter/DataCenter.cs
@@ -0,0 +1,186 @@
+﻿using Newtonsoft.Json.Linq;
+namespace EasyData
+{
+    public interface IDataNodeListener
+    {
+        void OnAddChild(DataNode node);
+        void OnDataNodeChange(DataNode node);
+        void OnDeleteChild(DataNode node);
+    }
+
+    public class DataCenter
+    {
+        private readonly DataNode m_Root = new("/");
+
+        public bool GetDataNode(string path, out DataNode node)
+        {
+            var paths = path.Split("/");
+            var currentNode = m_Root;
+            foreach (var item in paths.Where(str => !string.IsNullOrEmpty(str)))
+            {
+
+                currentNode = currentNode.GetChild(item);
+                if (currentNode == null)
+                {
+                    node = null;
+                    return false;
+                }
+            }
+            node = currentNode;
+            return true;
+        }
+
+        public void LoadFromJson(string path, JToken token)
+        {
+            var subPath = "";
+            switch (token.Type)
+            {
+                case JTokenType.Object:
+                    foreach (var item in token as JObject)
+                    {
+                        subPath = $"{path}/{item.Key}";
+                        LoadFromJson(subPath, item.Value);
+                    }
+                    break;
+                case JTokenType.Array:
+                    var array = token as JArray;
+                    for (var i = 0; i < array?.Count; i++)
+                    {
+                        subPath = $"{path}/{i}";
+                        LoadFromJson(subPath, array[i]);
+                    }
+                    break;
+                case JTokenType.Property:
+                    var property = token as JProperty;
+                    subPath = $"{path}/{property?.Name}";
+                    var data = new DataBase() { Value = property?.Value };
+                    var node = new DataNode(property?.Name, data);
+                    AddOrUpdateDataNode(subPath, node);
+                    break;
+                default:
+                    var str = Path.GetFileName(path);
+                    AddOrUpdateDataNode(path, new DataNode(str, new DataBase() { Value = token }));
+                    break;
+            }
+        }
+        public bool AddOrUpdateDataNode(string path, DataNode node)
+        {
+            if (GetDataNode(path, out var currentNode))
+            {
+                Console.WriteLine("当前节点已经存在");
+                currentNode.UpdateDataNode(node.Data);
+                return false;
+            }
+            else
+            {
+                var checkNode = m_Root;
+                path = path.TrimEnd(node.Name.ToCharArray());
+                var paths = path.Split("/");
+
+                foreach (var item in paths.Where(str => !string.IsNullOrEmpty(str)))
+                {
+                    var cNode = checkNode.GetChild(item);
+                    if (cNode == null)
+                    {
+                        cNode = new DataNode(item);
+                        checkNode.AddChild(cNode);
+                    }
+                    checkNode = cNode;
+                }
+                checkNode.AddChild(node);
+
+            }
+
+            return true;
+        }
+
+        public bool DeleteDataNode(string path)
+        {
+            if (GetDataNode(path, out var node))
+            {
+                node.DeleteDataNode();
+                return true;
+            }
+            return false;
+        }
+
+    }
+    public class DataBase
+    {
+        public object Value;
+    }
+    public class DataNode
+    {
+        private readonly List<IDataNodeListener> m_NodeListeners;
+
+        public DataNode(string name)
+        {
+            this.Name = name;
+            m_NodeListeners = new List<IDataNodeListener>();
+            m_Children = new Dictionary<string, DataNode>();
+        }
+        public DataNode(string name, DataBase data)
+        {
+            Name = name;
+            m_NodeListeners = new List<IDataNodeListener>();
+            m_Children = new Dictionary<string, DataNode>();
+            Data = data;
+        }
+        public readonly string Name;
+        private DataNode m_Parent;
+        private readonly Dictionary<string, DataNode> m_Children;
+        public DataBase Data;
+
+        public void AddChild(DataNode node)
+        {
+            node.m_Parent = this;
+            m_Children.Add(node.Name, node);
+            foreach (var listener in m_NodeListeners)
+            {
+                listener.OnAddChild(node);
+            }
+        }
+
+        public void AddNodeListener(IDataNodeListener listener)
+        {
+            m_NodeListeners.Add(listener);
+        }
+
+        public void RemoveNodeListener(IDataNodeListener listener)
+        {
+            m_NodeListeners.Remove(listener);
+        }
+
+        private void DeleteChild(DataNode node)
+        {
+            m_Children.Remove(node.Name);
+            foreach (var listener in m_NodeListeners)
+            {
+                listener.OnDeleteChild(node);
+            }
+        }
+        public DataNode GetChild(string childName)
+        {
+            m_Children.TryGetValue(childName, out var child);
+            return child;
+        }
+
+        public void UpdateDataNode(DataBase dataBase)
+        {
+            Data = dataBase;
+            foreach (var listener in m_NodeListeners)
+            {
+                listener.OnDataNodeChange(this);
+            }
+        }
+
+        public void DeleteDataNode()
+        {
+            m_Parent.DeleteChild(this);
+            foreach (var child in m_Children.Values)
+            {
+                child.DeleteDataNode();
+            }
+        }
+    }
+    }
---
+++ DataNodeCenter/Program.cs
@@ -0,0 +1,55 @@
+﻿// See https://aka.ms/new-console-template for more information
+using EasyData;
+using Newtonsoft.Json.Linq;
+
+Console.WriteLine("Hello, World!");
+DataCenter dataCenter = new DataCenter();
+var dataNode = new DataNode("c", new DataBase() { Value = 10 });
+var listener = new TestListener();
+dataNode.AddNodeListener(listener);
+dataCenter.AddOrUpdateDataNode("/a/b/c", dataNode);
+var d = dataCenter.GetDataNode("/a/b/c", out var node);
+node.UpdateDataNode(new DataBase() { Value = 11 });
+
+dataCenter.AddOrUpdateDataNode("a/b/c/d", new DataNode("d", new DataBase() { Value = 12 }));
+
+
+Console.WriteLine($"{d} {node.Data.Value}");
+node.UpdateDataNode(new DataBase() { Value = 10 });
+dataCenter.DeleteDataNode("a/b/c/d");
+
+node.RemoveNodeListener(listener);
+JToken token = new JObject()
+{
+    ["a"] = new JObject()
+    {
+        ["a1"] = 10,
+        ["a2"] = new JArray() { 11, 12, 13, 14, 15 }
+    },
+    ["b"] = 10,
+    ["c"] = new JArray() { 1, 2, 3, 4, 5 },
+    ["d"] = false
+};
+dataCenter.DeleteDataNode("a/b/c/d");
+dataCenter.LoadFromJson("/", token);
+if (dataCenter.GetDataNode("a/a2/2", out var node1))
+{
+    Console.WriteLine(node1.Data.Value);
+}
+public class TestListener : IDataNodeListener
+{
+    public void OnAddChild(DataNode node)
+    {
+        Console.WriteLine($"添加子节点{node.Name}");
+    }
+
+    public void OnDataNodeChange(DataNode node)
+    {
+        Console.WriteLine($"更新数据{node.Data.Value}");
+    }
+
+    public void OnDeleteChild(DataNode node)
+    {
+        Console.WriteLine($"删除子节点{node.Name}");
+    }
+}
